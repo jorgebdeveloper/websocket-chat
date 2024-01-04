dc05db2f103573d471cb26ec971eb2fef476db90
jorgebdeveloper <jorgebdeveloper@gmail.com>
2024-01-04 20:21:54 +0800

    init commit

---
+++ src/main/java/com/jorgeb/websocket/Message.java
@@ -0,0 +1,15 @@
+package com.jorgeb.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    private String text;
+    private String to;
+
+}
